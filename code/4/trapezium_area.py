c3705f190a933f1f38a92d0c70949915584467a1
Sungchul Choi <sc82.choi@gachon.ac.kr>
2017-10-29 16:26:37 +0900

    Add new exmaples for chapter 4 & 5

---
+++ code/4/trapezium_area.py
@@ -0,0 +1,5 @@
+def addtition(x,y):
+    return x+y
+
+if __name__ == "__main__":
+    print(addtition(10,5))
---
+++ code/5/calculate_rectangle.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_area(x,y):
+    return x*y
+
+rectangle_x = 10
+rectangle_y = 20
+print(calculate_rectangle_area(
+    rectangle_x, rectangle_y))
