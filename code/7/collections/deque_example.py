49a5c74ae188a89f0a15abc644fc29614d43d256
Sungchul Choi <sc82.choi@gachon.ac.kr>
2017-11-24 18:09:19 +0900

    Add deque examples

---
+++ code/7/collections/deque_example.py
@@ -0,0 +1,26 @@
+from collections import deque
+import time
+
+start_time = time.clock()
+deque_list = deque()
+for i in range(5):
+    deque_list.append(i)
+print(deque_list)
+
+deque_list.appendleft(10)
+print(deque_list)
+
+deque_list.rotate(2)
+print(deque_list)
+deque_list.rotate(2)
+print(deque_list)
+
+print(deque_list)
+print(deque(reversed(deque_list)))
+
+deque_list.extend([5, 6, 7])
+print(deque_list)
+
+deque_list.extendleft([5, 6, 7])
+print(deque_list)
+dd
--- code/7/collections/general_list.py
+++ code/7/collections/general_list.py
@@ -3,12 +3,6 @@ import time
 
 start_time = time.clock()
 deque_list = deque()
-# Stack
-for i in range(10000):
-    for i in range(10000):
-        deque_list.append(i)
-        deque_list.pop()
-print (time.clock() - start_time, "seconds")
 
 start_time = time.clock()
 just_list = []
@@ -17,9 +11,3 @@ for i in range(10000):
         just_list.append(i)
         just_list.pop()
 print (time.clock() - start_time, "seconds")
-
-# d.appendleft('f')
-# d.popleft()
-# d.rotate(1)
-# deque(reversed(d))
-# d.extendleft('abc')          
